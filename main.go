670a4824d34ebfbc3934dac66236005972550fc8
Erwin Susanto <erwin.susanto@erajaya.com>
2022-11-14 00:02:10 +0700

    commit ldap

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+//config port
+const (
+	webServerPort = "9000"
+	baseURL       = "0.0.0.0:" + webServerPort
+)
+
+func main() {
+	log.SetReportCaller(true)
+	r := mux.NewRouter()
+	r.HandleFunc("/", handleRoute)
+	r.HandleFunc("/login", handleLogin)
+
+	log.Infoln("Listening at ", baseURL)
+	log.Fatal(http.ListenAndServe(baseURL, r))
+}
+
+func handleRoute(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("login_template.html")
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	username := r.PostFormValue("username")
+	password := r.PostFormValue("password")
+
+	//authenticate
+	ok, data, err := AuthUsingLDAP(username, password)
+	if !ok {
+		http.Error(w, "invalid username/password", http.StatusUnauthorized)
+		return
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	message := fmt.Sprintf("Welcome %s\n", data.FullName)
+	w.Write([]byte(message))
+}
